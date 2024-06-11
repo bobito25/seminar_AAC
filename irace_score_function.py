cafa31c118adc0bfff34ba47c3731c2a3387d0a8
bobito25 <hammbob25@gmail.com>
2024-06-11 17:26:36 +0200

    init

---
+++ irace_score_function.py
@@ -0,0 +1,43 @@
+# https://github.com/SergioSJS/python-irace/tree/master
+
+import argparse
+import logging
+import sys
+
+from solve_tsp import solve_tsp_from_file
+
+def main(POP, TNRMT, CXPB, MUTPB, DATFILE):
+	# just a test
+	#score = MUTPB*POP/100
+	#score = float(score)
+	#score = score - float(CXPB)
+	
+	#score = float((POP-1)**2 + (CXPB-2)**2 + (MUTPB-3)**2 + 1)
+	#if score < 0:
+	#	score = 0
+	score = solve_tsp_from_file(params={"population_size": POP, "tournament_selection_size": TNRMT, "mutation_rate": MUTPB, "crossover_rate": CXPB, "target": 1.0}, filename="../genetic_tsp/TSP.txt")
+
+	# save the fo values in DATFILE
+	with open(DATFILE, 'w') as f:
+		f.write(str(score))
+
+if __name__ == "__main__":
+	# just check if args are ok
+	with open('args.txt', 'w') as f:
+		f.write(str(sys.argv))
+	
+	# loading example arguments
+	ap = argparse.ArgumentParser(description='Feature Selection using GA with DecisionTreeClassifier')
+	ap.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+	# 3 args to test values
+	ap.add_argument('--pop', dest='pop', type=int, required=True, help='Population size')
+	ap.add_argument('--tnrmt', dest='tnrmt', type=float, required=True, help='Tournament selection size')
+	ap.add_argument('--mut', dest='mut', type=float, required=True, help='Mutation probability')
+	ap.add_argument('--cros', dest='cros', type=float, required=True, help='Crossover probability')
+	# 1 arg file name to save and load fo value
+	ap.add_argument('--datfile', dest='datfile', type=str, required=True, help='File where it will be save the score (result)')
+
+	args = ap.parse_args()
+	logging.debug(args)
+	# call main function passing args
+	main(args.pop, args.cros, args.mut, args.datfile)
---
+++ solve_tsp.py
@@ -0,0 +1,64 @@
+import random
+
+from genetic_tsp.tsp import selectPopulation, geneticAlgorithm, drawMap
+
+def setup_tsp(num_cities, max_x, max_y):
+    cities = []
+    for i in range(num_cities):
+        cities.append((i+1, random.randint(0,max_x), random.randint(0,max_y)))
+    return {"cities": cities}
+
+def write_tsp_to_file(problem: dict):
+    with open("genetic_tsp/TSP.txt", "w") as f:
+        for city in problem["cities"]:
+            f.write(f"{city[0]} {city[1]} {city[2]}\n")
+
+def solve_tsp(problem: dict, params: dict, print_output=False) -> int:
+    population_size = params["population_size"]
+    tournament_selection_size = params["tournament_selection_size"]
+    mutation_rate = params["mutation_rate"]
+    crossover_rate = params["crossover_rate"]
+    target = params["target"]
+
+    cities = problem["cities"]
+    firstPopulation, firstFitest = selectPopulation(cities, population_size)
+    answer, genNumber = geneticAlgorithm(
+        firstPopulation,
+        len(cities),
+        tournament_selection_size,
+        mutation_rate,
+        crossover_rate,
+        target,
+    )
+    
+    if print_output:
+        print("\n----------------------------------------------------------------")
+        print("Generation: " + str(genNumber))
+        print("Fittest chromosome distance before training: " + str(firstFitest[0]))
+        print("Fittest chromosome distance after training: " + str(answer[0]))
+        print("Target distance: " + str(TARGET))
+        print("----------------------------------------------------------------\n")
+
+        drawMap(cities, answer)
+
+    return answer[0]
+
+def solve_tsp_from_file(params: dict, filename: str = "genetic_tsp/TSP.txt") -> int:
+    with open(filename, "r") as f:
+        cities = []
+        for line in f.readlines():
+            city = line.split()
+            cities.append((city[0], float(city[1]), float(city[2])))
+        problem = {"cities": cities}
+    return solve_tsp(problem, params)
+
+POPULATION_SIZE = 2000
+TOURNAMENT_SELECTION_SIZE = 4
+MUTATION_RATE = 0.1
+CROSSOVER_RATE = 0.9
+TARGET = 100.0
+
+TSP = setup_tsp(5, 10, 10)
+#write_tsp_to_file(TSP)
+#answer = solve_tsp_from_file({"population_size": POPULATION_SIZE, "tournament_selection_size": TOURNAMENT_SELECTION_SIZE, "mutation_rate": MUTATION_RATE, "crossover_rate": CROSSOVER_RATE, "target": TARGET})
+#print(answer)
